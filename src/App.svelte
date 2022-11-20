9adeb6af61551bf48cbbb4030955132c03dac019
trevo <trevorius45@gmail.com>
2022-11-20 14:59:47 -0500

    use props

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,14 @@
   import DisplayToggler from './first components/DisplayToggler.svelte';
   import Counter from './first components/Counter.svelte';
 
-  let counter = 0;
-  let min = 20;
-  let max = 70;
-  let incrementValue = 5;
+  let counterProps = {
+    counter: 0,
+    min: 20,
+    max: 70,
+    incrementValue: 5,
+  };
 </script>
 
-<Counter {counter} {min} {max} {incrementValue} />
+<Counter {...counterProps} />
 
 <DisplayToggler />
