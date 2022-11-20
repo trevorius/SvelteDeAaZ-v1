593c99978b55d2ace759b5b4cde0c918b4a97ad8
trevo <trevorius45@gmail.com>
2022-11-20 14:39:35 -0500

    moved component  to its own file

---
+++ src/first components/DisplayToggler.svelte
@@ -0,0 +1,36 @@
+<script>
+  let toggle = true;
+
+  const toggleDisplay = () => {
+    toggle = !toggle;
+  };
+</script>
+
+<button on:click={toggleDisplay}>{toggle === false ? 'Display' : 'Hide'}</button
+>
+
+{#if toggle}
+  <div class="show">
+    <h2>contents to display or not</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam molestiae
+      officiis voluptas neque accusamus nihil recusandae necessitatibus cumque
+      rerum. Similique!
+    </p>
+  </div>
+{/if}
+
+<style>
+  .show {
+    display: block;
+    width: 25%;
+    margin: auto;
+    padding: 1em;
+    background-color: black;
+    color: white;
+    box-shadow: 10px 5px 5px lightgrey;
+  }
+  .hide {
+    display: none;
+  }
+</style>
