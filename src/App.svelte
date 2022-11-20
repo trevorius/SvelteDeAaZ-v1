510d614ea93f298d60ceff04213ec28e8278c632
trevo <trevorius45@gmail.com>
2022-11-20 14:58:04 -0500

    Counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
   import DisplayToggler from './first components/DisplayToggler.svelte';
+  import Counter from './first components/Counter.svelte';
+
+  let counter = 0;
+  let min = 20;
+  let max = 70;
+  let incrementValue = 5;
 </script>
+
+<Counter {counter} {min} {max} {incrementValue} />
 
 <DisplayToggler />
---
+++ src/first components/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let counter = 0;
+  export let min = 30;
+  export let max = 80;
+  export let incrementValue = 10;
+
+  const increment = () => {
+    counter += incrementValue;
+  };
+</script>
+
+<button on:click={increment}> +{incrementValue} </button>
+
+<h2>{counter}</h2>
+
+{#if counter < min}
+  <p>counter less than {min}</p>
+{:else if counter >= min && counter <= max}
+  <p>counter is between {min} and {max}</p>
+{:else}
+  <p>
+    counter is above {max}
+  </p>
+{/if}
