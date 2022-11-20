437b7586cdd4b999dfebe092a74c677064acd206
trevo <trevorius45@gmail.com>
2022-11-20 14:33:15 -0500

    display hide logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,33 @@
 <script>
+  let toggle = true;
+
+  const toggleDisplay = () => {
+    toggle = !toggle;
+  };
 </script>
 
+<button on:click={toggleDisplay}>{toggle === false ? 'Display' : 'Hide'}</button
+>
+<div class={toggle ? 'show' : 'hide'}>
+  <h2>contents to display or not</h2>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam molestiae
+    officiis voluptas neque accusamus nihil recusandae necessitatibus cumque
+    rerum. Similique!
+  </p>
+</div>
+
 <style>
+  .show {
+    display: block;
+    width: 25%;
+    margin: auto;
+    padding: 1em;
+    background-color: black;
+    color: white;
+    box-shadow: 10px 5px 5px lightgrey;
+  }
+  .hide {
+    display: none;
+  }
 </style>
