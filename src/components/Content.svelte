144d5967c26fa427a72a4f0056c3ec2d76b68f1c
trevo <trevorius45@gmail.com>
2022-11-27 13:53:52 -0500

    content

---
+++ src/components/Content.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="content" />
+
+<style>
+  .content {
+    max-width: 1400px;
+    width: 95%;
+    padding: 0 50px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background: orange;
+  }
+</style>
