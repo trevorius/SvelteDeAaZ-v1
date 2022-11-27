e3e4a573510ed9d99b4272dd734b1f3b2569c907
trevo <trevorius45@gmail.com>
2022-11-27 15:01:16 -0500

    build data

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,6 @@
 
 <!-- <DarkLightApplied /> -->
 <NavBar />
-<SearchBar />
 <Content />
 
 <style>
--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,6 +1,44 @@
 <script>
+  import SearchBar from './SearchBar.svelte';
+
+  let allPokes = [];
+  let endTable = [];
+
+  // build data
+  const fecthPokemons = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then((response) => response.json())
+      .then((allPoke) => {
+        allPoke.results.forEach((pokemon) => {
+          fetchCompletePoke(pokemon);
+        });
+      });
+  };
+  fecthPokemons();
+  const fetchCompletePoke = (pokemon) => {
+    let fullPokemon = {};
+    let url = pokemon.url;
+    let name = pokemon.name;
+    fullPokemon.name = name;
+    fetch(url)
+      .then((response) => response.json())
+      .then((pokeData) => {
+        fullPokemon.pic = pokeData.sprites.back_default;
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}/`)
+          .then((response) => response.json())
+          .then((pokeData) => {
+            // console.log(pokeData);
+            fullPokemon.nameFr = pokeData.names[4];
+            allPokes.push(fullPokemon);
+          })
+          .then(() => {
+            console.log(allPokes);
+          });
+      });
+  };
 </script>
 
+<SearchBar />
 <div class="content" />
 
 <style>
