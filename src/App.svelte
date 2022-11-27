8a96d7d098e489b2d27af25eba73cfb0627cec5f
trevo <trevorius45@gmail.com>
2022-11-27 13:53:58 -0500

    content

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   // import DarkLightApplied from './first components/DarkLightApplied.svelte';
   import NavBar from './components/NavBar.svelte';
+  import Content from './components/Content.svelte';
 </script>
 
 <!-- <DarkLightApplied /> -->
 <NavBar />
+<Content />
 
 <style>
 </style>
