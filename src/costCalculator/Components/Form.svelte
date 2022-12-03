4e5c5ce0ab2a0e9e3d563d10277db2d6f58735d7
trevo <trevorius45@gmail.com>
2022-12-03 15:20:49 -0500

    basic elements

---
+++ src/costCalculator/Components/Form.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<form action="" class="my-5">
+  <div class="form-group">
+    <label for="expense" class="lead fw-bold"> Expense name </label>
+    <input
+      type="text"
+      id="expense"
+      class="form-control"
+      placeholder=" Enter a new expense"
+    />
+  </div>
+  <div class="form-group">
+    <label for="amount" class="lead fw-bold"> Amount</label>
+    <input
+      type="text"
+      id="amount"
+      class="form-control"
+      placeholder=" Enter  the amount"
+    />
+  </div>
+  <button type="submit" class="btn btn-primary m-2">Add the expense</button>
+</form>
--- src/costCalculator/CostCalculator.svelte
+++ src/costCalculator/CostCalculator.svelte
@@ -1,5 +1,10 @@
 <script>
+  import NavBar from './Components/NavBar.svelte';
+  import Content from './Components/Content.svelte';
 </script>
+
+<NavBar />
+<Content />
 
 <style>
 </style>
