057289826bdb0dc14e121e7fe95cae3a5155a905
trevo <trevorius45@gmail.com>
2022-12-03 15:21:05 -0500

    content

---
+++ src/costCalculator/Components/Content.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Form from './Form.svelte';
+  import ExpenseCard from './ExpenseCard.svelte';
+</script>
+
+<div class="container">
+  <Form />
+  <h2 class="my-4">Total Expenses : xxxx</h2>
+  <ExpenseCard />
+</div>
