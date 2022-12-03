d76d95d55831241a90380a1c891d24df18cbef19
trevo <trevorius45@gmail.com>
2022-12-03 14:40:36 -0500

    :tada: setup

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,11 @@
   // import AppComponents from './first components/AppComponents.svelte';
   // import DarkLightApplied from './first components/DarkLightApplied.svelte';
   // import AppPokedex from './pokedex/AppPokedex.svelte';
+  import CostCalculator from './costCalculator/CostCalculator.svelte';
+  import Card from './pokedex/contentElements/Card.svelte';
 </script>
 
+<CostCalculator />
 <!-- <AppComponents /> -->
 <!-- <DarkLightApplied /> -->
 
