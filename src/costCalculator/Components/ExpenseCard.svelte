1049e10e57b29b795c9cba453f9c0aaa4d75cd2d
trevo <trevorius45@gmail.com>
2022-12-03 15:21:14 -0500

    svg and card

---
+++ src/costCalculator/Components/ExpenseCard.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+<div class="card">
+  <div class="card-body">
+    <h3>Car</h3>
+    <p>200</p>
+    <i class="trash">
+      <img src="assets/image/trash-solid.svg" alt="" />
+    </i>
+  </div>
+</div>
